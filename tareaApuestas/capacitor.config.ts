483577cf5870e10078eba52fc044c9e4276515fd
Alberto2804 <amelchorj01@educarex.es>
2026-02-16 09:59:43 +0100

    first commit

---
+++ tareaApuestas/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'tareaApuestas',
+  webDir: 'www'
+};
+
+export default config;
